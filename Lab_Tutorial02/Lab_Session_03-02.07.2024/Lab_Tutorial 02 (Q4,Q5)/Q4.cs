d46d38d1db2f9250631f86c32b9be761b9bbcd2d
kavindyadesilva <135606798+kavindyadesilva@users.noreply.github.com>
2024-02-14 15:19:31 +0530

    I have completed the whole tutorial 02 with 4 & 5 questions

---
+++ Lab_Tutorial02/Lab_Session_03-02.07.2024/Lab_Tutorial 02 (Q4,Q5)/Q4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_02__Q4_
+{
+     class Product
+    {
+        public string ProductName;
+        public double Price;
+
+        public Product(string productName,double price)
+        {
+            ProductName = productName;
+            Price = price;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("laptop", 200000);
+            Product product2 = new Product("smartphone", 50000);
+
+            Console.WriteLine(product1);
+            Console.WriteLine("Name:" + product1.ProductName);
+            Console.WriteLine("Price:$" + product1.Price);
+            Console.WriteLine();
+
+            Console.WriteLine(product2);
+            Console.WriteLine("Name:" + product2.ProductName);
+            Console.WriteLine("Price:$" + product2.Price);
+            Console.ReadLine();
+        }
+    }
+        
+}
---
+++ Lab_Tutorial02/Lab_Session_03-02.07.2024/Lab_Tutorial 02 (Q4,Q5)/Q5.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial_02__Q5_
+{
+    class LibraryBook
+
+    {
+        public string Title;
+        public string Author;
+        public bool Available;
+
+            public LibraryBook(string title,string author)
+        {
+            Title = title;
+            Author = author;
+            Available = true;
+        }
+        public void BorrowBook()
+        {
+            if(Available)
+            {
+                Available = false;
+                Console.WriteLine($"Book'{Title}' by{Author} has been borrowed");
+            }
+            else
+            {
+                Console.WriteLine($"book'{Title}' by {Author} is not available");
+            }
+        }
+    }
+    class Library
+    {
+        public List<LibraryBook> Books;
+
+        public Library()
+        {
+            Books = new List<LibraryBook>();
+        }
+        public void AddBook(LibraryBook book)
+        {
+            Books.Add(book);
+        }
+        public void DisplayLibraryStatus()
+        {
+            Console.WriteLine("Library Status");
+            foreach(var book in Books)
+            {
+                Console.WriteLine($"Title:{book.Title},Author:{book.Author},Available:{(book.Available ? "yes":"no")}");
+
+            }
+        }
+    }
+    class Program
+    {
+      static void Main(string[]args)
+        {
+            Library library = new Library();
+
+            library.AddBook(new LibraryBook("Nectar in a Sieve", "Kamala Markandaya"));
+            library.AddBook(new LibraryBook("Bringing Tony Home", "Tissa Abeysekara"));
+            library.AddBook(new LibraryBook("Life of Pi", "Yann Martel"));
+
+            library.DisplayLibraryStatus();
+
+            library.Books[0].BorrowBook();
+
+            library.DisplayLibraryStatus();
+
+            Console.ReadLine();
+        }
+    }
+}
